fa5c932412126436ea56d2bf573609a2fd5e2737
Cristian Toaquiza <crissmtl@gmail.com>
2016-02-11 11:20:05 -0500

    Añadir API de Usuarioy Amigo

---
+++ api/controllers/AmigoController.js
@@ -0,0 +1,11 @@
+/**
+ * AmigoController
+ *
+ * @description :: Server-side logic for managing Amigoes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
